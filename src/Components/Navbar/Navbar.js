28211133d5d7167d60d8d7ebe807c0e7ddef3f97
Arcadia-B <by.yunusdeniz@gmail.com>
2024-11-04 13:22:05 +0300

    edited navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,26 +2,33 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-500'>
+    <nav className=' bg-white fixed z-50 w-full '> 
         <div className='container mx-auto'>
-      {/* <ul className='flex space-x-4 text-white justify-center text-2xl'> */}
-        {/* <li>
-            <a href="#home">Home</a>
+      <ul className='flex space-x-4  justify-center text-2xl '>
+        <li>
+            <a className='' href="#home">Home</a>
         </li>
+        <span>|</span>
         <li>
             <a href="#about">About</a>
+            
         </li>
+        <span>|</span>
         <li>
             <a href="#supplements">Supplements</a>
         </li>
+        <span>|</span>
         <li>
             <a href="#contact">Contact</a>
         </li>
+        <span>|</span>
         <li>
             <a href="#support">Support</a>
         </li>
-      </ul> */}
+        
+      </ul>
       </div>
+      <div className='border-b-4 border-purple-500'></div>
        
     </nav>
   )
