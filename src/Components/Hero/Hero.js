2b88fe220d4ff1330b6a4a7f694d5fc95482dcf0
Arcadia-B <by.yunusdeniz@gmail.com>
2024-11-04 12:14:18 +0300

    newsectionadded

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import hero from "../../Assets/hero.jpg"
+
+const Hero = () => {
+  return (
+    <div className='relative'>
+        <img className='w-full' src={hero} alt="hero_img" />
+        <div className='absolute inset-0 bg-black opacity-50'></div>
+        <div className='absolute inset-0 flex flex-col justify-center items-center text-center top-20'>
+            <p className=' text-white mb-4 text-3xl max-w-md'>You don’t have to go it alone when it comes to fitness!
+                 Take advantage of our 20% 
+                discount on couple memberships and embark on a healthy journey with your partner.</p>
+                   <button className='text-white bg-purple-500 rounded-lg px-4 py-2'>See Offers</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,9 +2,28 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className='bg-gray-500'>
+        <div className='container mx-auto'>
+      {/* <ul className='flex space-x-4 text-white justify-center text-2xl'> */}
+        {/* <li>
+            <a href="#home">Home</a>
+        </li>
+        <li>
+            <a href="#about">About</a>
+        </li>
+        <li>
+            <a href="#supplements">Supplements</a>
+        </li>
+        <li>
+            <a href="#contact">Contact</a>
+        </li>
+        <li>
+            <a href="#support">Support</a>
+        </li>
+      </ul> */}
+      </div>
        
-    </div>
+    </nav>
   )
 }
 
