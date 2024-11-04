794af8110a9dc7149aeabe78f5cc1bbcdb34e070
Arcadia-B <by.yunusdeniz@gmail.com>
2024-11-04 22:52:50 +0300

    navbarchanged

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,23 +9,23 @@ const Navbar = () => {
           <img src={logo} alt="sitelogo" className='h-20 mr-4' /> 
         </div>
         <ul className='flex space-x-4 text-2xl'>
-          <li className='hover:bg-purple-500 hover:text-white'>
+          <li className='hover:text-purple-500 hover:border-b-2 hover:border-purple-500 '>
             <a href="#home">HOME</a>
           </li>
           <span>|</span>
-          <li>
+          <li className='hover:text-purple-500 hover:border-b-2 hover:border-purple-500 '  >
             <a href="#about">ABOUT</a>
           </li>
           <span>|</span>
-          <li>
+          <li className='hover:text-purple-500 hover:border-b-2 hover:border-purple-500 '>
             <a href="#supplements">SUPPLEMENTS</a>
           </li>
           <span>|</span>
-          <li>
+          <li className='hover:text-purple-500 hover:border-b-2 hover:border-purple-500 '>
             <a href="#contact">CONTACT</a>
           </li>
           <span>|</span>
-          <li>
+          <li className='hover:text-purple-500 hover:border-b-2 hover:border-purple-500 '>
             <a href="#support">SUPPORT</a>
           </li>
         </ul>
