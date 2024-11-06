d4b9e47b175ff74c2314138af33d6890ba32f196
Arcadia-B <by.yunusdeniz@gmail.com>
2024-11-06 11:46:03 +0300

    exercises section responsive improved

--- src/Components/Exercises/Exercises.js
+++ src/Components/Exercises/Exercises.js
@@ -5,23 +5,23 @@ import exercise_3 from "../../Assets/exercise-3.jpg"
 
 const Exercises = () => {
   return (
-    <div className='mx-auto container'>
-        <div className='flex justify-between gap-6 mt-10 '>
-            <div className=''>
+    <div className='mx-auto container px-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10'>
+            <div className='flex flex-col justify-center'>
         <img src={exercise_1} alt="exercise_img" className='w-full rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-lg hover:opacity-90 cursor-pointer' />
-        <h1 className='text-3xl'>HeavyLifting</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat pariatur 
+        <h1 className='text-2xl sm:text-3xl mt-4'>HeavyLifting</h1>
+        <p className='text-sm sm:text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat pariatur 
           aspernatur officia animi nesciunt.</p>
         </div>
         <div >
         <img src={exercise_2} alt="exercise_img" className='w-full rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-lg hover:opacity-90 cursor-pointer ' />
-        <h1 className='text-3xl'>Cardio</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, illum? Nam inventore magnam autem adipisci?</p>
+        <h1 className='text-2xl sm:text-3xl mt-4'>Cardio</h1>
+        <p className='text-sm sm:text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, illum? Nam inventore magnam autem adipisci?</p>
         </div>
         <div>
         <img src={exercise_3} alt="exercise_img" className='w-full rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-lg hover:opacity-90 cursor-pointer ' />
-        <h1 className='text-3xl'>Meditation</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio id aliquid ducimus debitis atque praesentium?</p>
+        <h1 className='text-2xl sm:text-3xl mt-4'>Meditation</h1>
+        <p className='text-sm sm:text-base mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio id aliquid ducimus debitis atque praesentium?</p>
         </div>
         </div>
 
